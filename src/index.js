d7baa4dbe66fd8503bebcdb83b36bfe2348e46f1
A Groshek <amy@amygroshek.com>
2020-10-21 13:54:08 -0500

    Export core components

--- src/index.js
+++ src/index.js
@@ -5,3 +5,14 @@ import { Explorer } from './modules/cpal'
 import './styles/style.scss'
 
 export default Explorer
+
+// Export core components.
+export { default as Header } from './modules/core/Header'
+export { default as Logo } from './modules/core/Logo'
+export { default as CoreButton } from './modules/core/CoreButton'
+export { default as Search } from './modules/core/Search'
+export { default as Canvas } from './modules/core/Canvas'
+export { default as View } from './modules/core/View'
+export { default as Divider } from './modules/core/Divider'
+export { default as Select } from './modules/core/Select'
+export { default as TourIcon } from './modules/core/Icons/TourIcon'
